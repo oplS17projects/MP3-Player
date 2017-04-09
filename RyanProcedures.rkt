64a0984c51573c2621ec69d35d5aac1e4372deca
Ryan DeLosh <ryguy1201@gmail.com>
2017-04-09 19:10:27 -0400

    Milestone 1 Commit -Ryan

--- RyanProcedures.rkt
+++ RyanProcedures.rkt
@@ -3,9 +3,40 @@
 
 (define my-vlc (start-vlc #:port 5000 #:hostname "127.0.0.1"))
 (vlc-loop #t)
-(define isShuffle #f)
-(define isPlaying #f)
 (define nil '())
+
+;Creates a state object that we can later use to keep track of true/false states
+(define (make-state Statenow)
+
+  (define (change-state)
+    (if (eq? Statenow #f)
+        (set! Statenow #t)
+        (set! Statenow #f)))
+
+  (define (setF)
+    (set! Statenow #f))
+
+  (define (setT)
+    (set! Statenow #t))
+  
+  (define (getState) Statenow)
+  
+  (define (dispatch m)
+            (cond ((eq? m 'flip) (change-state))
+                  ((eq? m 'false) (setF))
+                  ((eq? m 'true) (setT))
+                  ((eq? m 'state?) (getState))
+                  (else (error "Unknown request" m))))
+  dispatch)
+;;interface with state using
+;;(statename 'flip) toggle between true/false
+;;(statename 'false) set that state to false
+;;(statename 'true) set that state to true
+;;(statename 'state?) retrive the value of the state
+
+(define isShuffle (make-state #f))
+(define isPlaying (make-state #f))
+
 ;;Play Now
 ;;;Must be able to adapt to every song will add to top of queue and play now depending on what vlc supports
 (define (playNow URL)
@@ -14,16 +45,14 @@
 
 ;;Song Pause/unpause
 (define (pause) (begin (vlc-pause)
-                 (if (eq? isPlaying #t)
-                     (set! isPlaying #f)
-                     (set! isPlaying #t))))
+                 (isPlaying 'flip)))
 
 ;;Song Next in Queue
 (define (myNext) (vlc-next))
 ;;Song Previous
 (define (myPrev) (vlc-prev))
 ;;currentlyPlaying?
-(define (isPlaying?) isPlaying)
+(define (isPlaying?) (isPlaying 'state?))
 
 ;;Shuffle All
 (define (shuffleAll lst)
@@ -32,22 +61,25 @@
 
 (define (shuffleToggle)
   (if (eq? isShuffle #t)
-       (begin (vlc-random #f) (set! isShuffle #f))
-       (begin (vlc-random #t) (set! isShuffle #t))))
+       (begin (vlc-random #f) (isShuffle 'false))
+       (begin (vlc-random #t) (isShuffle 'true))))
 ;;Play All
 ;;;Adds all to queue without shuffling
 (define (playAll lst)
   (if (eq? lst nil)
       'ok
-      (begin(addQ (car lst))(set! isPlaying #t)(playAll (cdr lst)))))
+      (begin(addQ (car lst))(isPlaying 'true)(playAll (cdr lst)))))
 ;;Add to Queue
 ;;;Must be able to adapt to every song
 (define (addQ URL) (begin (vlc-add URL)
-                    (set! isPlaying #t)))
+                          (if (eq? #f (isPlaying 'state?))
+                              (begin (pause) (isPlaying 'false))
+                              (isPlaying 'true))))
 ;;Clear Queue
 (define (clearQ) (vlc-clear))
-;;/Users/liqueseous/ownCloud/Documents/Spring2017/OPL/MP3-Player/Media/Hypnotic.mp3
-;;(define mylist '("/Users/liqueseous/ownCloud/Documents/Spring2017/OPL/MP3-Player/Media/NEST.mp3""/Users/liqueseous/ownCloud/Documents/Spring2017/OPL/MP3-Player/Media/Victorious.mp3""/Users/liqueseous/ownCloud/Documents/Spring2017/OPL/MP3-Player/Media/Hypnotic.mp3"))
+;;;USED FOR LOCAL TESTING
+;;/Users/liqueseous/ownCloud/Documents/Spring2017/OPL/MP3-Player/TestMedia/Hypnotic.mp3
+;;(define mylist '("/Users/liqueseous/ownCloud/Documents/Spring2017/OPL/MP3-Player/TestMedia/NEST.mp3""/Users/liqueseous/ownCloud/Documents/Spring2017/OPL/MP3-Player/TestMedia/Victorious.mp3""/Users/liqueseous/ownCloud/Documents/Spring2017/OPL/MP3-Player/TestMedia/Hypnotic.mp3"))
 
 
 
